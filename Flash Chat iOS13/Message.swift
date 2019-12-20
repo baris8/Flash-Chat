8b62dfc79cc16adc886fbbe92932b74500a0ee76
uectas <baris.uectas@uni-duesseldorf.de>
2019-12-20 19:32:11 +0100

    finished project

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Baris Uectas on 20.12.19.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //Sender Email
+    let body: String
+}
